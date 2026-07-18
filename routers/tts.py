d928fcc9d14f3ba09cfac9b6fe8461de67d7993b
gpojarvis-sudo <gpojarvis@gmail.com>
2026-07-18 13:25:50 +0530

    Create TTS router

--- routers/tts.py
+++ routers/tts.py
@@ -1,1 +1,13 @@
-# TTS Router
+from fastapi import APIRouter
+from models.request import TTSRequest
+
+router = APIRouter()
+
+
+@router.post("/tts")
+async def tts(req: TTSRequest):
+    return {
+        "success": True,
+        "received_text": req.text,
+        "length": len(req.text)
+    }
